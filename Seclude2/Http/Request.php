4749e230c56cf0e9ee34563da32f1874cf79841e
Xesau <aron2000@zeelandnet.nl>
2015-06-26 19:44:59 +0200

    Http\Request, will be used in router

---
+++ Seclude2/Http/Request.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Seclude2\Http;
+
+class Request {
+    
+    private $method;
+    private $path;
+    private $secure;
+    
+    public function __construct ($method, $path, $secure) {
+        $this->method = $method;
+        $this->path  = $path;
+        $this->secure = $secure;
+    }
+    
+    public static function fromGlobals () {
+        return new Request ($_SERVER['REQUEST_METHOD'], $_SERVER ['PHP_SELF'], isset ($_SERVER['HTTPS']));
+    }
+    
+    public function getMethod () {
+        return $this->method;
+    }
+    
+    public function getPath () {
+        return $this->path;
+    }
+    
+    public function isSecure () {
+        return $this->secure;
+    }
+    
+}
