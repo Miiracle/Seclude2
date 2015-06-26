359a0409246abe9b949ff63b2d2271a1877ffe37
Xesau <Xesau@users.noreply.github.com>
2015-06-26 10:09:13 +0200

    1. Autoloader
    The automatically loads classes.
    All the classes that belong to Seclude2 are loaded from the directory the script is in.
    Every other class is loaded from the defined Seclude2\LIB_DIR

---
+++ Seclude/Autoloader.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Seclude2;
+
+if (!defined ('Seclude2\LIB_DIR')) {
+    trigger_error ('No Seclude2\LIB_DIR defined. Using /vendor');
+    define ('Seclude2\LIB_DIR', realpath (__DIR__ .DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'vendor'));
+}
+
+spl_autoload_register(function ($className) {
+    // Normalizer function, replacecs / and \ with DIRECTORY_SEPARATOR
+    $normalize = function ($path) { return str_replace(array ('\\','/'), DIRECTORY_SEPARATOR, $path); };
+    
+    // Seclude2 classes are loaded form __DIR__
+    if (strlen($className) >= 9 && substr ($className, 0, 9) == 'Seclude2\\')
+        $fn = __DIR__ . DIRECTORY_SEPARATOR . $normalize(substr ($className, 9).'.php');
+    
+    // All the other things are loaded from the to-be-defined LIB_DIR
+    else
+        $fn = LIB_DIR . DIRECTORY_SEPARATOR . $normalize($className.'.php');
+    
+    // If the file is not a directory nor a link and is readable
+    if (is_file($fn) && is_readable($fn))
+        @include_once $fn;
+});
